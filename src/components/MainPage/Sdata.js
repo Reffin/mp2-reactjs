4512b36697e98859d19052f1adbd9e046e4cdb8d
Reffin <ryancarbonel1984@gmail.com>
2023-02-21 14:34:18 +0800

    Update

--- src/components/MainPage/Sdata.js
+++ src/components/MainPage/Sdata.js
@@ -3,25 +3,25 @@ const Sdata = [
     id: 1,
     title: "50% Off For Your First Shopping",
     desc: "ASUS ROG Strix G15 (2022) Gaming Laptop, 15.6 300Hz IPS FHD Display, NVIDIA GeForce RTX 3050, AMD Ryzen 7 6800H, 16GB DDR5, 1TB SSD, RGB Keyboard, Windows 11 Home, G513RC- IS74",
-    cover: "./images/SlideCard/slide-1.png",
+    cover: "./images/SlideCard/slide1.png",
   },
   {
     id: 2,
     title: "50% Off For Your First Shopping",
     desc: "This Picture Of Leather Varsity Jacket - Purple And White Varsity Jacket is high quality ",
-    cover: "./images/SlideCard//slide-2.png",
+    cover: "./images/SlideCard//slide2.png",
   },
   {
     id: 3,
     title: "50% Off For Your First Shopping",
     desc: "Gregory Baltoro Tourist Bag Gregory Men's Baltoro 75 Pack Cinder Cone Red",
-    cover: "./images/SlideCard/slide-3.png",
+    cover: "./images/SlideCard/slide3.png",
   },
   {
     id: 4,
     title: "50% Off For Your First Shopping",
     desc: "This Air Force 1 Low Mini Swoosh 'university Red' - Nike Shoes Air Force 1 is high quality",
-    cover: "./images/SlideCard/slide-4.png",
+    cover: "./images/SlideCard/slide4.png",
   },
 ]
 export default Sdata
