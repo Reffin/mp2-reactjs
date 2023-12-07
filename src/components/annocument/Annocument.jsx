110c031bfc4ac1577ab089a0ee05b513ac5a2b60
Ryan <ryancarbonel1984@gmail.com>
2023-12-07 14:06:03 +0800

    initial commit

--- src/components/annocument/Annocument.jsx
+++ src/components/annocument/Annocument.jsx
@@ -17,7 +17,7 @@ const Annocument = () => {
             <img src='/images/banner-1.png' width='100%' height='100%' alt="" />
           </div>
           <div className='img' style={mystyle1}>
-            <img src='/images/banner-3.jpg' width='100%' height='100%' alt="" />
+            <img src='/images/banner-3.png' width='100%' height='100%' alt="" />
           </div>
         </div>
       </section>
